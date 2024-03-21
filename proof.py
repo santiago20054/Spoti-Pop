d04be2910afe36d9b870e4c46f39b4d1a40f92c2
santiago20054 <56911524+santiago20054@users.noreply.github.com>
2024-03-21 00:17:58 -0500

    tercera prueba

--- proof.py
+++ proof.py
@@ -4,7 +4,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
 import time
-dotenv_path = r'C:\Users\santi\OneDrive\Desktop\Spotify_Popularity\.env.txt'
+dotenv_path = r'C:\Users\MAFISH\Documents\spoti\Spoti-Pop\.env.txt'
 load_dotenv(dotenv_path)
 
 # Configura tus credenciales de cliente
@@ -17,80 +17,88 @@ print(client_id)
 
 # Obtener el ID de la playlist
 dfs = []
-#playlist_id = ['1M31YfWCBmOSlGGyF0wvsw', '37i9dQZF1DX4KeocBrdbJg', '37i9dQZEVXbMDoHDwVN2tF', '37i9dQZF1DXbvPjXfc8G9S',
-#              '4KlHX1H41ZIyQ5oodAp2kj', '7xwYHh7KbKDtlMblSkvtfu']
-
-playlist_id = ['7xwYHh7KbKDtlMblSkvtfu']
-playlist_info = sp.playlist('7xwYHh7KbKDtlMblSkvtfu')
-total_tracks = playlist_info['tracks']['total']
-print(f"La lista de reproducción tiene {total_tracks} canciones.")
+playlist_id = ['1M31YfWCBmOSlGGyF0wvsw', '37i9dQZF1DX4KeocBrdbJg', '37i9dQZEVXbMDoHDwVN2tF', '37i9dQZF1DXbvPjXfc8G9S',
+              '4KlHX1H41ZIyQ5oodAp2kj', '7xwYHh7KbKDtlMblSkvtfu']
 
 
-def divisible_by_100_list(x):
-    closest_below = (x // 100) * 100
-    return list(range(0, closest_below + 1, 100))
 
-listaplaylist = divisible_by_100_list(total_tracks)
+for j in playlist_id:
 
-for i in listaplaylist:
+    playlist_info = sp.playlist(j)
+    total_tracks = playlist_info['tracks']['total']
+    print(f"La lista de reproducción tiene {total_tracks} canciones.")
 
-    for pl in playlist_id:
-        # Obtener las canciones de la playlist
-        results = sp.playlist_tracks(pl,offset =i)
 
-        #################################################################################3
+    def divisible_by_100_list(x):
+        closest_below = (x // 100) * 100
+        return list(range(0, closest_below + 1, 100))
 
-        # Listas para almacenar los datos
-        song_names = []
-        song_ids = []
-        #audio_features = []
-        popularity = []
-        artists = []
-        release_years = []
-        
-        # Iterar sobre las canciones y obtener los datos
-        for track in results['items']:
-            # Obtener datos de la canción
-            song_name = track['track']['name']
-            song_id = track['track']['id']
-            print(f"Processing song: {song_name}, ID: {song_id}")  # Add this line for debugging
-            if song_id:
-                song_names.append(song_name)
-                song_ids.append(song_id)
-                time.sleep(0.1)
-                # Obtener popularidad de la canción
-                song_popularity = track['track']['popularity']
-                popularity.append(song_popularity)
+    listaplaylist = divisible_by_100_list(total_tracks)
+
+    for i in listaplaylist:
+
+        for pl in playlist_id:
+            # Obtener las canciones de la playlist
+            results = sp.playlist_tracks(pl,offset =i)
             
-                # Obtener género de la canción
-                artist_info = track['track']['artists'][0]  # Obtener la información del primer artista de la canción
-                artist_name = artist_info['name']
-                artists.append(artist_name)
-            
-                # Obtener año de lanzamiento de la canción
-                track_info = sp.track(song_id)
-                release_date = track_info['album']['release_date']
-                release_year = release_date.split('-')[0] if release_date else None
-                release_years.append(release_year)
-            else:
-                print(f"Skipping song: {song_name}, ID is None")  # Add this line for debugging
+            #################################################################################3
 
-    
-        # Crear DataFrame con los datos
-        df = pd.DataFrame({
-            'Song ID': song_ids,
-            'Song Name': song_names,
-            'Artist Name': artists,
-            'Year': release_years,
-            'Popularity': popularity
-        })
+            # Listas para almacenar los datos
+            song_names = []
+            song_ids = []
+            #audio_features = []
+            popularity = []
+            artists = []
+            release_years = []
+            
+            # Iterar sobre las canciones y obtener los datos
+            for track in results['items']:
+                time.sleep(31)
+                # Obtener datos de la canción
+                song_name = track['track']['name']
+                song_id = track['track']['id']
+                print(f"Processing song: {song_name}, ID: {song_id}")  # Add this line for debugging
+                if song_id:
+                    song_names.append(song_name)
+                    song_ids.append(song_id)
+                    # Obtener popularidad de la canción
+                    song_popularity = track['track']['popularity']
+                    popularity.append(song_popularity)
+
+                    #ADUIO FEATUREEEEEEEEEEEES
+                    #features = sp.audio_features([song_id])[0]
+                    #audio_features.append(features)
+                
+                    # Obtener género de la canción
+                    artist_info = track['track']['artists'][0]  # Obtener la información del primer artista de la canción
+                    artist_name = artist_info['name']
+                    artists.append(artist_name)
+                
+                    # Obtener año de lanzamiento de la canción
+                    track_info = sp.track(song_id)
+                    release_date = track_info['album']['release_date']
+                    release_year = release_date.split('-')[0] if release_date else None
+                    release_years.append(release_year)
+                else:
+                    print(f"Skipping song: {song_name}, ID is None")  # Add this line for debugging
+
         
-        #df_audio_features = pd.DataFrame(audio_features)
-        #df_completo = pd.merge(df, df_audio_features, how='left', left_on='Song ID', right_on='id')
-        #df_completo = df_completo.drop(columns=['type', 'id', 'uri', 'track_href', 'analysis_url'], axis=1)
-        dfs.append(df)
+            # Crear DataFrame con los datos
+            df = pd.DataFrame({
+                'Song ID': song_ids,
+                'Song Name': song_names,
+                'Artist Name': artists,
+                'Year': release_years,
+                'Popularity': popularity
+            })
+            
+            #df_audio_features = pd.DataFrame(audio_features)
+            #df_completo = pd.merge(df, df_audio_features, how='left', left_on='Song ID', right_on='id')
+            #df_completo = df_completo.drop(columns=['type', 'id', 'uri', 'track_href', 'analysis_url'], axis=1)
+            dfs.append(df)
 
 
 tablon = pd.concat(dfs)
 tablon = tablon.drop_duplicates(subset='Song ID').reset_index(drop=True)
+tablon.to_csv("TABLON1.csv",index=False)
 print(tablon)
